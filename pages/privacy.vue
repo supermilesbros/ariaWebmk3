71c0e760f9210f6d7592235b85e8b1c974a2b605
SuperMilesBros <jared@supermilesbros.com>
2020-11-10 15:10:30 -0500

    dfas

---
+++ pages/privacy.vue
@@ -0,0 +1,250 @@
+<template>
+  <div class="main-container">
+    <HeaderAria />
+    <div class="blog-pages-wrapper section-space--ptb_100">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 ml-auto mr-auto">
+            <div class="main-blog-wrap">
+              <!--======= Single Blog Item Start ========-->
+              <div class="single-blog-item  wow move-up">
+                <!-- Post info Start -->
+                <div class="post-info lg-blog-post-info">
+                  <h3 class="post-title text-center">
+                    Terms and Conditions
+                  </h3>
+
+                  <div class="post-excerpt mt-10">
+                    <p>
+                      ARIA Diagnostics LLC requires all visitors to this site to agree to these terms and conditions. If you do
+                      not agree, you should not use this site. By accessing the site you indicate your acknowledgment and acceptance of these terms and conditions.
+                    </p>
+                    <p>
+                      Restrictions on the use of the text, images, graphics, sound files, animation, video and/or their arrangement on the ARIA Diagnostics LLC website are all subject to intellectual property right protection and
+                      such material from www.ariadxs.com and any other World Wide Web site owned, operated, licensed or
+                      controlled by ARIA Diagnostics LLC or any of its related, affiliated or subsidiary companies (collectively
+                      ARIA Diagnostics LLC). may not be copied, distributed, republished, uploaded, posted or transmitted in
+                      any way without the prior written consent of ARIA Diagnostics LLC. Modification or use of the materials
+                      for any other purpose violates ARIA Diagnostics LLC intellectual property rights. Neither title nor intellectual property rights are transferred to you by access to this site, but remain with ARIA Diagnostics LLC,
+                      who owns full and complete title to the material in this site.
+                    </p>
+                    <h4>Submissions</h4>
+                    <p>
+                      All remarks, suggestions, ideas, graphics, or other information communicated to ARIA Diagnostics
+                      LLC through this site (collectively the “Submission”) will forever be the property of ARIA Diagnostics
+                      LLC. ARIA Diagnostics LLC will not be required to treat any Submission as confidential, will not be liable
+                      for any ideas for its business (including without limitation, product or advertising ideas) and will not incur
+                      any liability as a result of any similarities that may appear in future ARIA Diagnostics LLC operations.
+                      Without limitation, ARIA Diagnostics LLC will have the exclusive ownership of all present and future existing rights to the Submission of every kind and nature everywhere. ARIA Diagnostics LLC will be entitled
+                      to use the Submission for any commercial or other purposes whatsoever without compensation to you or
+                      other persons sending a Submission. By access to this site you acknowledge that you are responsible for
+                      whatever material you submit, and you, not ARIA Diagnostics LLC have full responsibility for the message, including its legality, reliability, appropriateness, originality and copyright. ARIA Diagnostics
+                      LLC does not warrant or make any representation regarding the use of the materials contained in this site
+                      in terms of correctness, accuracy, and reliability or otherwise. The material in this site may include technical inaccuracies or typographical errors and ARIA Diagnostics LLC may make changes or improvements
+                      at any time.
+                    </p>
+                    <h4>No Warranties or Representations</h4>
+                    <p>
+                      The information contained in this site is provided “as is” and without warranty of any kind, expressed or
+                      implied, including, but not limited to, all warranties of merchantability and fitness for a particular purpose. ARIA Diagnostics LLC does not warrant that the functions contained in the material will be uninterrupted or error free, that defects will be corrected, or that this site or the server that makes it available
+                      are free of viruses or other harmful components. ARIA Diagnostics LLC does not warrant or make any
+                      representations regarding the use of or the result of the use of the material in this site in terms of their
+                      correctness, accuracy, reliability, or otherwise. You (and not ARIA Diagnostics LLC) assume the entire cost
+                      of all necessary servicing, repair or correction. Accordingly, by using this information you agree that ARIA
+                      Diagnostics LLC (including its agents, officers, directors and employees) are not liable for damages of
+                      any kind.
+                    </p>
+                    <h4>Miscellaneous</h4>
+                    <p>
+                      These terms and conditions will be governed and interpreted pursuant to the laws of the State of Indiana, inside the United States of America, notwithstanding any principles of conflict of laws. This is the
+                      entire agreement between the parties relating to the use of this site. ARIA Diagnostics LLC can revise
+                      these terms and conditions at any time by updating this posting. ARIA Diagnostics LLC products and
+                      services are available in many parts of the United States. However, the ARIA Diagnostics LLC site may
+                      describe products and services that are not available nationally. This site may be linked to other sites,
+                      which are not maintained by ARIA Diagnostics LLC. ARIA Diagnostics LLC is not responsible for the content of those sites.
+                    </p>
+                    <p>
+                      All trademarks are the property of their respective trademark owners.
+                      There may be occasions when ARIA Diagnostics LLC obtains personally identifiable information to provide the visitor with correspondence or otherwise. However, ARIA Diagnostics LLC only gathers personally identifiable information, such as names, addresses, e-mail addresses, etc., when a visitor voluntarily
+                      submits the same. Such personally identifiable information, which may be gathered in connection with
+                      ARIA Diagnostics LLC on-line visitors, is used solely by ARIA Diagnostics LLC and is not sold, transferred
+                      or otherwise disseminated to third parties outside ARIA Diagnostics LLC.
+                    </p>
+                    <h4>Parents</h4>
+                    <p>
+                      ARIA Diagnostics LLC strongly encourages all parents to supervise their children’s online activities and
+                      regularly monitors their children’s use of the Internet. Parents should consider using parental control
+                      tools and other services to assist them in supervising their children’s on-line use or on-line disclosure of
+                      their name, address or other personal information without parental consent.
+                    </p>
+                    <p>
+                      ARIA Diagnostics LLC reserves the right to change this policy at any time. Please check this page periodically for changes.
+                    </p>
+                    <h4>Parents</h4>
+                    <p>
+                      ARIA Diagnostics LLC strongly encourages all parents to supervise their children’s online activities and
+                      regularly monitors their children’s use of the Internet. Parents should consider using parental control
+                      tools and other services to assist them in supervising their children’s on-line use or on-line disclosure of
+                      their name, address or other personal information without parental consent.
+                    </p>
+                    <p>
+                      ARIA Diagnostics LLC reserves the right to change this policy at any time. Please check this page periodically for changes.
+                    </p>
+
+                    <p>Source: Aria Diagnostics</p>
+                  </div>
+                </div>
+                <div class="post-info lg-blog-post-info">
+                  <h3 class="post-title text-center">
+                    Privacy Policy
+                  </h3>
+
+                  <div class="post-excerpt mt-10">
+                    <p>
+                      ARIA Diagnostics LLC is pleased to provide information to all of its customers about its online privacy
+                      policy. Please be assured that ARIA Diagnostics LLC uses its best efforts to protect the privacy of visitors
+                      to this web site.
+                    </p>
+                    <h4>Types of Information We Collect and How It Is Used</h4>
+                    <p>
+                      ARIA Diagnostics LLC only gathers personal information, such as, first and last names, street addresses,
+                      and email addresses, when voluntarily submitted by you. Personal information will be collected from you
+                      only in order to distribute information you request.
+                    </p>
+                    <p>
+                      With your permission, we will use personal information collected from you to send you information. If
+                      you do not elect to receive such material, we will not send it to you.
+                    </p>
+                    <h4>Sharing of Personal Information</h4>
+                    <p>
+                      Companies may be engaged by ARIA Diagnostics LLC to perform a variety of functions, such as, providing technical services for our web sites, etc. These companies may have access to personal information if
+                      needed to perform such functions. However, these companies may only use such personal information
+                      for the purpose of performing that function and may not use it for any other purpose. Other than as provided in this privacy policy, ARIA Diagnostics LLC does not sell, transfer, or disclose personal information
+                      to third parties.
+                    </p>
+                    <p>
+                      ARIA Diagnostics LLC reserves the right to use or disclose any information as needed to satisfy any law,
+                      regulation, or legal request, to protect the integrity of the site, to fulfill your requests, or to cooperate in
+                      any law enforcement investigation or an investigation on a matter of public safety.
+                    </p>
+                    <h4>Access</h4>
+                    <p>
+                      You are in control of any personal information you provide to us online. If at any time, you would like to
+                      correct the personal information we have about you or if you would like to change your preferences for
+                      contacts from us, you can let us know by contacting us at the postal address listed below.
+                    </p>
+                    <h4>Cookies</h4>
+                    <p>
+                      “Cookies” are pieces of information that a web site transfers to an individual’s hard drive for recordkeeping purposes. Cookies allow the website to remember important information that will make your
+                      use of the site more convenient. Like most web sites, www.ariadxs.com uses cookies for a variety of purposes in order to improve your online experience. For example, we track the total number of visitors to
+                      our site on an anonymous aggregate basis. We may also employ cookies so that we remember you when
+                      you return to the site. We may associate personal information with a cookie file in that instance
+                    </p>
+                    <p>
+                      Use the options in your web browser if you do not wish to receive a cookie or if you wish to set your
+                      browser to notify you when you receive a cookie. Click on the “Help” section of your browser to learn
+                      how to change your cookie preferences. If you disable all cookies, you may not be able to take advantage of all the features of this web site.
+                    </p>
+                    <h4>Links to Other Sites</h4>
+                    <p>
+                      We may offer links to sites. ARIA Diagnostics LLC is not responsible for the policies and practices of other companies and any information not operated by ARIA Diagnostics LLC. If you visit one of these linked
+                      sites, you should review their privacy and other policies. We are not responsible for the policies and
+                      practices of other companies.
+                    </p>
+                    <h4>Changes to our Privacy Policy </h4>
+                    <p>
+                      This privacy policy is effective as of September 28th, 2015. From time to time, it may be necessary for
+                      ARIA Diagnostics LLC to change this privacy policy. If we change our policy, we will post the revised version here, so we suggest that you check here periodically for the most up-to-date version of our privacy
+                      policy. Rest assured, however, that any changes will not be retroactively applied and will not alter how we
+                      handle previously collected information.
+                    </p>
+                    <p>Source: Aria Diagnostics</p>
+                  </div>
+                </div>
+                <div class="post-info lg-blog-post-info">
+                  <h3 class="post-title text-center">
+                    Privacy (HIPAA) Information
+                  </h3>
+
+                  <div class="post-excerpt mt-10">
+                    <p>
+                      ARIA Diagnostics LLC makes all reasonable efforts to comply with HIPAA (the Health Insurance Portability
+                      and Accountability Act of 1996) and privacy regulations. We take these matters very seriously and have
+                      instituted numerous internal and external measures to maximize compliance and mitigate risk.
+                    </p>
+                    <p>
+                      With the rapidly changing legislative and regulatory impacts on the health care environment, ARIA Diagnostics LLC and its subsidiaries recognize that corporations want assurance that our organization is actively assessing our compliance with the Health Insurance Portability and Accountability Act (HIPAA).
+                      ARIA Diagnostics LLC continues to implement companywide strategies to address HIPAA. Our goal is to
+                      protect the privacy and security of individually identifiable health information and our clients’ ability to
+                      use our services.
+                    </p>
+                    <p>
+                      ARIA Diagnostics LLC maintains a Regulatory & Risk Mitigation Officer dedicated to tracking all proposed, pending, and active legislative and regulatory developments that are directly proportionate to
+                      ARIA Diagnostics LLC and our customers’ business needs. A primary focus of this department is to research privacy and security regulations and legislation on both the federal and state level. We are actively involved in the impact assessment of HIPAA on ARIA Diagnostics LLC business units
+                    </p>
+                    <p>
+                      ARIA Diagnostics LLC has a Privacy and Security Task Force that incorporates HIPAA and consists of individuals from the following departments: Legal, Regulatory, Finance, Human Resources, Information
+                      Technology, Risk Management, Medical, and Business Operations.
+                    </p>
+                    <p>
+                      ARIA Diagnostics LLC is addressing the communication of new laws relative to privacy and security
+                      through a corporate-driven HIPAA awareness program. This program consists of education of management and colleagues through the ARIA Diagnostics LLC internal compliance and education program.
+                      This is mandatory to ensure participation. ARIA Diagnostics LLC remains confident that the effort put
+                      forth will produce the results necessary to achieve our compliance goals. ARIA Diagnostics LLC Regulatory & Risk Mitigation Officer welcomes the opportunity to hear from you regarding your HIPAA questions and concerns. We know that starting a dialogue with our customers will only prove to strengthen
+                      our mutual HIPAA compliance efforts
+                    </p>
+                  </div>
+                </div>
+                <!-- Post info End -->
+              </div>
+              <!--===== Single Blog Item End =========-->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <AriaFooter />
+
+    <OffCanvasMobileMenu />
+  </div>
+</template>
+
+<script>
+import data from '../data/blog.json'
+import HeaderAria from '@/components/aria/HeaderAria'
+import AriaFooter from '@/components/aria/AriaFooter'
+import OffCanvasMobileMenu from '@/components/OffCanvasMobileMenu'
+
+export default {
+  components: {
+    HeaderAria,
+    AriaFooter,
+    OffCanvasMobileMenu
+  },
+  data () {
+    return {
+      data,
+      breadcrumbNavigation: [
+        {
+          text: 'Home',
+          to: '/'
+        },
+        {
+          text: 'Blogs',
+          to: '/blog'
+        },
+        {
+          text: 'Blog Details',
+          active: true
+        }
+      ]
+    }
+  },
+  head () {
+    return {
+      title: 'Blog Details'
+    }
+  }
+}
+</script>
