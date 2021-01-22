6693d421d6b21294c851be88b6a79bba5bfd2b1a
SuperMilesBros <jared@supermilesbros.com>
2021-01-22 14:55:28 -0500

    lolz this is pretty dumb

---
+++ pages/woodymyers.vue
@@ -0,0 +1,44 @@
+
+<template>
+  <div class="main-container">
+    <HeaderAria />
+
+    <ContactUsForm />
+
+    <OffCanvasMobileMenu />
+  </div>
+</template>
+
+<script>
+import HeaderAria from '@/components/aria/HeaderAria'
+import ContactUsForm from '@/components/aria/sub-components/TestingContactForm'
+import OffCanvasMobileMenu from '@/components/OffCanvasMobileMenu'
+
+export default {
+  name: 'Contact',
+  components: {
+    HeaderAria,
+    ContactUsForm,
+    OffCanvasMobileMenu
+  },
+  data () {
+    return {
+      breadcrumbNavigation: [
+        {
+          text: 'Home',
+          to: '/'
+        },
+        {
+          text: 'Contact Us',
+          active: true
+        }
+      ]
+    }
+  },
+  head () {
+    return {
+      title: 'Contact Us'
+    }
+  }
+}
+</script>
