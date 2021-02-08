84a121e37763cb3f72c75e67b174acfb7406a052
SuperMilesBros <jared@supermilesbros.com>
2021-02-07 20:00:32 -0500

    sdf

--- pages/pcrscheduler.vue
+++ pages/pcrscheduler.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <!-- Calendly inline widget begin -->
-    no-ssr
+    <client-only>
     <div class="calendly-inline-widget" data-url="https://calendly.com/aria-pcrtest" style="min-width:320px;height:630px;" />
     <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" />
     <!-- Calendly inline widget end -->
+    </client-only>
   </div>
 </template>
 
