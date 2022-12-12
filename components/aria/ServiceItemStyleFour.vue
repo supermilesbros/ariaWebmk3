89ad7455e0e52ef3ee0bf972a96a9c52c47b716a
SuperMilesBros <66653712+supermilesbros@users.noreply.github.com>
2022-12-12 11:05:25 -0500

    changes mk3

--- components/aria/ServiceItemStyleFour.vue
+++ components/aria/ServiceItemStyleFour.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- <n-link :to="`/${service.link}`" class="ht-box-images style-02">
+  <n-link :to="`/${service.link}`" class="ht-box-images style-02">
     <div class="image-box-wrap">
       <div class="box-image">
         <img class="img-fulid" :src="service.icon" alt="icon">
@@ -10,8 +10,8 @@
         </h6>
       </div>
     </div>
-  </n-link> -->
-  <div class="ht-box-images style-02">
+  </n-link>
+  <!-- <div class="ht-box-images style-02">
     <div class="image-box-wrap">
       <div class="box-image">
         <img class="img-fulid" :src="service.icon" alt="icon">
@@ -22,7 +22,7 @@
         </h6>
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
