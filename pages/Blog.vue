2a65ba96df61767daf5ad071c33ac991c20ccd16
SuperMilesBros <66653712+supermilesbros@users.noreply.github.com>
2023-03-13 13:32:57 -0400

    changes to blog

--- pages/Blog.vue
+++ pages/Blog.vue
@@ -5,21 +5,23 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-12">
-            <AriaSectionTitle class="text-center section-space--mb_40" title="Blog Articles" sub-title="Aria Diagnostics" />
+            <AriaSectionTitle
+              class="text-center section-space--mb_40"
+              title="Blog Articles"
+              sub-title="Aria Diagnostics"
+            />
           </div>
         </div>
 
-        <div class="row ">
+        <div class="row">
           <div class="col-lg-6 col-md-6">
-            <n-link to="/BlogOne" class="projects-wrap style-01">
+            <n-link to="/POC-Advantages" class="projects-wrap style-01">
               <div class="projects-image-box">
                 <div class="content text-color-primary">
-                  <h6 class="heading">
-                    POC Advantages
-                  </h6>
+                  <h6 class="heading">POC Advantages</h6>
                   <div class="text">
-                    &quot;In-office diagnostic testing allows for more personalized and targeted
-                    treatment...&quot;
+                    &quot;In-office diagnostic testing allows for more
+                    personalized and targeted treatment...&quot;
                   </div>
                   <div class="box-projects-arrow">
                     <span class="button-text">View </span>
@@ -33,11 +35,10 @@
             <n-link to="/BlogTwo" class="projects-wrap style-01">
               <div class="projects-image-box">
                 <div class="content text-color-primary">
-                  <h6 class="heading">
-                    Centralized Labs
-                  </h6>
+                  <h6 class="heading">Centralized Labs</h6>
                   <div class="text">
-                    &quot;Standardization is key to ensuring the accuracy and reliability of clinical trial data....&quot;
+                    &quot;Standardization is key to ensuring the accuracy and
+                    reliability of clinical trial data....&quot;
                   </div>
                   <div class="box-projects-arrow">
                     <span class="button-text">View </span>
@@ -55,21 +56,21 @@
 </template>
 
 <script>
-import data from '../data/resources.json'
-import AriaSectionTitle from '@/components/aria/sub-components/AriaSectionTitle'
-import HeaderAria from '@/components/aria/HeaderAria'
-import AriaFooter from '@/components/aria/AriaFooter'
+import data from "../data/resources.json";
+import AriaSectionTitle from "@/components/aria/sub-components/AriaSectionTitle";
+import HeaderAria from "@/components/aria/HeaderAria";
+import AriaFooter from "@/components/aria/AriaFooter";
 
 export default {
   components: {
     AriaSectionTitle,
     HeaderAria,
-    AriaFooter
+    AriaFooter,
   },
-  data () {
+  data() {
     return {
-      data
-    }
-  }
-}
+      data,
+    };
+  },
+};
 </script>
