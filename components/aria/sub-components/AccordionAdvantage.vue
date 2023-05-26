82b0e6a0999a7ccb1254fe6349104747f370f493
SuperMilesBros <66653712+supermilesbros@users.noreply.github.com>
2023-05-26 09:25:43 -0400

    sadsd

--- components/aria/sub-components/AccordionAdvantage.vue
+++ components/aria/sub-components/AccordionAdvantage.vue
@@ -3,27 +3,6 @@
     <b-card no-body>
       <b-card-header header-tag="header">
         <h5 class="mb-0 font-weight--bold">
-          <b-button v-b-toggle.accordion-one block class="btn-link">
-            Comprehensive Drug Testing
-            <span>
-              <i class="far fa-caret-circle-down" />
-              <i class="far fa-caret-circle-right" />
-            </span>
-          </b-button>
-        </h5>
-      </b-card-header>
-      <b-collapse id="accordion-one" accordion="my-accordion">
-        <b-card-body>
-          <b-card-text>
-            ARIA can test for over 120+ analytes. ARIA provides extensive testing for pain management, natural/semi-synthetic/synthetic opioids & opiates, antipsychotics, illicits (like MDMA, PCP, Ketamine & THC) and upon request designer drugs (like synthetic cannabinoids, synthetic cathinones, etc). ARIA offers Presumptive and Definitive assays to ensure the most comprehensive and accurate testing available. Our assays are continually updated as new drugs are introduced or usage of a particular drug spikes. ARIA uses Liquid Chromatography coupled with Tandem Mass Spectrometry (LC-MS/MS) to ensure any positive or unexpected presumptive tests are verified for accuracy and cross-reactivity. Our clients rely on accurate results to implement thorough compliance adherence so ARIA believes in utilizing the highest quality equipment and standards. Any immunoassay presumptive test that reads positive or unexpected should be verified using additional technology like GC-MS, LC-MS or LC-MS/MS. At ARIA we use LC-MS/MS to ensure better specificity and precision identification. LC-MS/MS quantitates more drugs and metabolites, with greater accuracy by analyzing mass and structure.
-          </b-card-text>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
-
-    <b-card no-body>
-      <b-card-header header-tag="header">
-        <h5 class="mb-0 font-weight--bold">
           <b-button v-b-toggle.accordion-two block class="btn-link">
             Proven Test Accuracy
             <span>
@@ -36,7 +15,15 @@
       <b-collapse id="accordion-two" accordion="my-accordion">
         <b-card-body>
           <b-card-text>
-            ARIA Diagnostics combines Presumptive Immunoassay testing with Definitive LC-MS/MS testing to ensure the most accurate analysis possible. Following the specimen analyses, ARIA will certify the laboratory results. Results are reviewed to identify any false or unexpected results as well as uncovering any cross-reactivity interference from structurally similar substances. ARIA applies CAP standards to our Quality Control and Quality Assurance, documenting throughout testing to help physicians’ confidently and accurately maximize compliance and mitigate risk.
+            ARIA Diagnostics combines Presumptive Immunoassay testing with
+            Definitive LC-MS/MS testing to ensure the most accurate analysis
+            possible. Following the specimen analyses, ARIA will certify the
+            laboratory results. Results are reviewed to identify any false or
+            unexpected results as well as uncovering any cross-reactivity
+            interference from structurally similar substances. ARIA applies CAP
+            standards to our Quality Control and Quality Assurance, documenting
+            throughout testing to help physicians’ confidently and accurately
+            maximize compliance and mitigate risk.
           </b-card-text>
         </b-card-body>
       </b-collapse>
@@ -57,7 +44,11 @@
       <b-collapse id="accordion-three" accordion="my-accordion">
         <b-card-body>
           <b-card-text>
-            ARIA has trained specimen collectors upon request for observed and unobserved collection and courier services to help you accommodate the needs of our clients. Combined with Specimen Adulteration Testing, these added services help ensure your specimens are viable, preventing rejection.
+            ARIA has trained specimen collectors upon request for observed and
+            unobserved collection and courier services to help you accommodate
+            the needs of our clients. Combined with Specimen Adulteration
+            Testing, these added services help ensure your specimens are viable,
+            preventing rejection.
           </b-card-text>
         </b-card-body>
       </b-collapse>
@@ -78,7 +69,9 @@
       <b-collapse id="accordion-four" accordion="my-accordion">
         <b-card-body>
           <b-card-text>
-            ARIA offers assistance in developing compliance and testing protocol for many applications including pain management, adherence monitoring, rehab abstinence and workplace and student drug testing.
+            ARIA offers assistance in developing compliance and testing protocol
+            for many applications including pain management, adherence
+            monitoring, rehab abstinence and workplace and student drug testing.
           </b-card-text>
         </b-card-body>
       </b-collapse>
@@ -99,7 +92,17 @@
       <b-collapse id="accordion-five" accordion="my-accordion">
         <b-card-body>
           <b-card-text>
-            ARIA's StratusDX Laboratory Information System offers web, EMR and cloud-based reporting. Paperless reports can be delivered conveniently and securely to the client. Sample analysis results are delivered in a comprehensive and easy-to-interpret report that highlights positive and inconsistent results. Test results are released to clients after they are scrutinized and dual data review approved. Patient confidentiality is paramount throughout each phase of testing, and thus ARIA uses an encrypted, HIPAA compliant, customer specific web portal. ARIA certified technicians are available for consultation to clarify and explain the technical findings.
+            ARIA's StratusDX Laboratory Information System offers web, EMR and
+            cloud-based reporting. Paperless reports can be delivered
+            conveniently and securely to the client. Sample analysis results are
+            delivered in a comprehensive and easy-to-interpret report that
+            highlights positive and inconsistent results. Test results are
+            released to clients after they are scrutinized and dual data review
+            approved. Patient confidentiality is paramount throughout each phase
+            of testing, and thus ARIA uses an encrypted, HIPAA compliant,
+            customer specific web portal. ARIA certified technicians are
+            available for consultation to clarify and explain the technical
+            findings.
           </b-card-text>
         </b-card-body>
       </b-collapse>
@@ -120,7 +123,13 @@
       <b-collapse id="accordion-six" accordion="my-accordion">
         <b-card-body>
           <b-card-text>
-            Our greatest asset is our staff. Providing fast, accurate, and secure results requires highly qualified, professional lab personnel. In the field, we rely on our professional and motivated team of client service specialists to help you navigate testing needs. We apply CAP standards to quality control and quality assurance, documenting throughout testing to help clients' confidently and accurately maximize compliance and mitigate risk.
+            Our greatest asset is our staff. Providing fast, accurate, and
+            secure results requires highly qualified, professional lab
+            personnel. In the field, we rely on our professional and motivated
+            team of client service specialists to help you navigate testing
+            needs. We apply CAP standards to quality control and quality
+            assurance, documenting throughout testing to help clients'
+            confidently and accurately maximize compliance and mitigate risk.
           </b-card-text>
         </b-card-body>
       </b-collapse>
